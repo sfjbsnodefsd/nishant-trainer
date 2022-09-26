9da625334e0a00be94d2e38f37df9ba10f77f600
ngactive99 <ngactive99@gmail.com>
2022-09-26 20:07:21 +0530

    added put and delete method

--- node-js/Node-Rest-Api-Mongo/routes/courses.js
+++ node-js/Node-Rest-Api-Mongo/routes/courses.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const ex = require("express");
 const Course = require("../models/course");
 const router = ex.Router();
@@ -7,20 +8,45 @@ router.get("/allcourses", async (req, res) => {
   try {
     const courses = await Course.find();
     res.json(courses);
-  } catch(err) {
+  } catch (err) {
     res.json(err);
   }
 });
 
 // add a course
 
- router.post("/addcourse",async(req,res) => {
-    try{
-      const course= await Course.create(req.body);
-      res.json(course);
-    } catch (err) {
-        res.json(err)
-    }
- })
+router.post("/addcourse", async (req, res) => {
+  try {
+    const course = await Course.create(req.body);
+    res.json(course);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// delete method
+router.delete("/delete/:courseId", async (req, res) => {
+  try {
+    await Course.remove({ _id: req.params.courseId });
+    res.status(200).json({
+      message: "deleted sucessfully",
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// update course
+
+router.put("/update/:courseId", async (req, res) => {
+  const courseId = req.params.courseId;
+
+  try {
+    const course = await Course.updateOne({ _id: courseId }, req.body);
+    res.json(course);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 module.exports = router;
