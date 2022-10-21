910c9995cbf027575b60698998683be80fc4f0df
ngactive99 <ngactive99@gmail.com>
2022-10-21 19:00:18 +0530

    corrected few things

--- node-js/Rabbit-MQ-Microservices/producer/index.js
+++ node-js/Rabbit-MQ-Microservices/producer/index.js
@@ -2,22 +2,18 @@ const express = require("express");
 const app = express();
 const amqp = require("amqplib");
 var channel, connection;
-
-function connect() {
+async function connect() {
   try {
-    const amqpServer = "http://localhost:5672";
-    connection = amqp.connect(amqpServer);
-    channel = connection.createChannel();
-    channel.assertQueue("Rabbit");
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("Rabbit");
   } catch (err) {
     console.log(err);
   }
 }
-
 connect();
-
 app.get("/send", (req, res) => {});
-
-app.listen(5000, () => {
+app.listen(5001, () => {
   console.log("server is running at port 5000");
 });
