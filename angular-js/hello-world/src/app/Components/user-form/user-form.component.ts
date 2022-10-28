abc8f7472bd5ba926af62bb0b1d619373e09b951
ngactive99 <ngactive99@gmail.com>
2022-10-28 18:37:45 +0530

    post method implemented

--- angular-js/hello-world/src/app/Components/user-form/user-form.component.ts
+++ angular-js/hello-world/src/app/Components/user-form/user-form.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import User from 'src/app/Entity/User';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css']
+  styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent implements OnInit {
-title =  "Fill out the form below";
-// to access the data of this variable in out html file use {{variable name}}
-name = "";
-age= 0 ;
-gender = ""; 
+  title = 'Fill out the form below';
+  // to access the data of this variable in out html file use {{variable name}}
+  user: User = new User();
 
-
-save() {
-  console.log(this.name + " " + this.age + " " + this.gender);
-  
-}
-
-  constructor() { }
-
-  ngOnInit(): void {
+  save() {
+    const observables = this.userService.saveUser(this.user);
+    observables.subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
   }
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {}
 }
